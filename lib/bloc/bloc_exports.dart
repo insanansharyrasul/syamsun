2b8b8039644570a35347bfc4e61e87fe9454673a
insanansharyrasul <insanansryrasul21@gmail.com>
2026-01-03 20:54:42 +0700

    feat: removed unnecessary comment on export

--- lib/bloc/bloc_exports.dart
+++ lib/bloc/bloc_exports.dart
@@ -1,8 +1,3 @@
-// Permission BLoC
 export 'permission/permission_bloc.dart';
-
-// Settings BLoC
 export 'settings/settings_bloc.dart';
-
-// Prayer Times BLoC
 export 'prayer_times/prayer_times_bloc.dart';
